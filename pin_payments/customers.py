a0340ee9e2e4ce2f9ac2131b0efb7f2404d0f8a8
Bohdan Salamakha <allen.avanheim@gmail.com>
2023-12-11 19:23:17 +0200

    bump: version 0.1.0 → 0.2.0 FEAT 1. added commitizen lib 2. tested commitizen lib 3. working on customers.py module documentation

--- pin_payments/customers.py
+++ pin_payments/customers.py
@@ -10,160 +10,143 @@ class CustomersAPI:
     ):
         self.__api_key = api_key
 
-    def post_charges(
+    def post_customers(
             self,
             email: str,
-            description: str,
-            amount: int,
-            ip_address: str,
-            currency: Optional[str] = None,
-            capture: Optional[bool] = None,
-            reference: Optional[str] = None,
-            metadata: Optional[dict] = None,
-            three_d_secure: Optional[dict] = None,
-            platform_adjustment: Optional[dict] = None,
+            first_name: Optional[str] = None,
+            last_name: Optional[str] = None,
+            phone_number: Optional[str] = None,
+            company: Optional[str] = None,
+            notes: Optional[str] = None,
+            # need to use one of the following
+            card: Optional[dict] = None,
+            card_token: Optional[str] = None
+    ) -> None:
+        """
+        Creates a new customer and returns its details.
+
+        POST /customers
+
+        Example:
+        curl https://test-api.pinpayments.com/1/customers -u your-secret-api-key: \
+         -d "email=roland@pinpayments.com" \
+         -d "first_name=Roland" \
+         -d "last_name=Robot" \
+         -d "phone_number=1300 364 800" \
+         -d "company=Pin Payments" \
+         -d "notes=Account manager at Pin Payments" \
+         -d "card[number]=5520000000000000" \
+         -d "card[expiry_month]=05" \
+         -d "card[expiry_year]=2024" \
+         -d "card[cvc]=123" \
+         -d "card[name]=Roland Robot" \
+         -d "card[address_line1]=42 Sevenoaks St" \
+         -d "card[address_line2]=" \
+         -d "card[address_city]=Lathlain" \
+         -d "card[address_postcode]=6454" \
+         -d "card[address_state]=WA" \
+         -d "card[address_country]=Australia"
+
+        :param email: The email address of the customer.
+        :param first_name: The customer’s first name.
+        :param last_name: The customer’s surname.
+        :param phone_number: The customer’s contact number.
+        :param company: The company associated with the customer.
+        :param notes: Internal notes for the customer.
+        :param card: The full details of the payment card to be stored
+        :param card_token: The token of the card to be stored, as returned from the cards API or customers API.
+        :return: None
+        """
+        ...
+
+    def get_customers(
+            self
+    ) -> None:
+        """
+        Returns a paginated list of all customers.
+
+        GET /customers
+
+        Example:
+        curl https://test-api.pinpayments.com/1/customers -u your-secret-api-key:
+
+        :return: None
+        """
+        ...
+
+    def get_customers_customer_token(
+            self
+    ) -> None:
+        """
+        Returns the details of a customer.
+
+        GET /customers/customer-token
+
+        Example:
+        curl https://test-api.pinpayments.com/1/customers/cus_XZg1ULpWaROQCOT5PdwLkQ -u your-secret-api-key:
+
+        :return: None
+        """
+        ...
+
+    def put_customers_customer_token(
+            self,
+            email: Optional[str] = None,
+            first_name: Optional[str] = None,
+            last_name: Optional[str] = None,
+            phone_number: Optional[str] = None,
+            company: Optional[str] = None,
+            notes: Optional[str] = None,
             # need to use one of the following
             card: Optional[dict] = None,
             card_token: Optional[str] = None,
-            payment_source_token: Optional[str] = None,
-            customer_token: Optional[str] = None,
+            primary_card_token: Optional[str] = None,
     ) -> None:
         """
-        Creates a new charge and returns its details. This may be a long-running request.
+        Updates the details of a customer and returns the updated details. You can update the customer’s cards in one of four ways:
+        You can use the card[...] parameters to store a new card that will replace the customer’s primary card. The customer’s current primary card will be removed from storage and you will not be able to recover it.
+        You can use the card_token parameter to replace the customer’s primary card with a previously stored card. The card token must either be already associated with this customer record or unused. The customer’s current primary card will be removed from storage and you will not be able to recover it.
+        You can use the primary_card_token parameter to switch the customer’s primary card to a previously stored card. The card token must either be already associated with this customer record or unused. The current primary card will become a non-primary card of the customer.
+        You can use none of the above parameters. The customer’s cards will not change.
+        In addition, you can update the customer’s email address and contact details.
 
-        POST /charges
+        PUT /customers/customer-token
 
         Example:
-        curl https://test-api.pinpayments.com/1/charges -u your-secret-api-key: \
-        -d "amount=400" \
-        -d "currency=AUD" \
-        -d "description=test charge" \
-        -d "email=roland@pinpayments.com" \
-        -d "ip_address=203.192.1.172" \
-        -d "card[number]=5520000000000000" \
-        -d "card[expiry_month]=05" \
-        -d "card[expiry_year]=2024" \
-        -d "card[cvc]=123" \
-        -d "card[name]=Roland Robot" \
-        -d "card[address_line1]=42 Sevenoaks St" \
-        -d "card[address_line2]=" \
-        -d "card[address_city]=Lathlain" \
-        -d "card[address_postcode]=6454" \
-        -d "card[address_state]=WA" \
-        -d "card[address_country]=Australia" \
-        -d "metadata[OrderNumber]=123456" \
-        -d "metadata[CustomerName]=Roland Robot"
+        curl https://test-api.pinpayments.com/1/customers/cus_XZg1ULpWaROQCOT5PdwLkQ -u your-secret-api-key: -X PUT \
+         -d "email=roland@pinpayments.com" \
+         -d "first_name=Roland" \
+         -d "last_name=Robot" \
+         -d "phone_number=1300 364 800" \
+         -d "company=Pin Payments" \
+         -d "notes=Account manager at Pin Payments" \
+         -d "card[number]=5520000000000000" \
+         -d "card[expiry_month]=05" \
+         -d "card[expiry_year]=2024" \
+         -d "card[cvc]=123" \
+         -d "card[name]=Roland Robot" \
+         -d "card[address_line1]=42 Sevenoaks St" \
+         -d "card[address_line2]=" \
+         -d "card[address_city]=Lathlain" \
+         -d "card[address_postcode]=6454" \
+         -d "card[address_state]=WA" \
+         -d "card[address_country]=Australia"
 
-        :param email: The email address of the purchaser.
-        :param description: A description of the item purchased (e.g. 500g of single origin beans).
-        :param amount: The amount to charge in the currency’s base unit (e.g. cents for AUD, yen for JPY). There is a minimum charge amount for each currency; refer to the documentation on supported currencies.
-        :param ip_address: The IP address of the person submitting the payment.
-        :param currency: The three-character ISO 4217 currency code of one of our supported currencies, e.g. AUD or USD. Default value is AUD.
-        :param capture: Whether to immediately capture the charge (true or false). If false, we will attempt to create an authorisation; if this is successful, you can capture at a later time. Authorised charges automatically expire after seven days. Default value is true.
-        :param reference: A custom text string which will be displayed in place of the default descriptor on the customer's bank statement.
-        :param metadata: Arbitrary key-value data to be associated with the charge.
-        :param three_d_secure: Information required to enable 3D Secure on payments.
-        :param platform_adjustment: Specify an amount to withhold from the merchant entitlement to collect as revenue for your platform.
-        :param card: The full details of the payment card to be charged
-        :param card_token: The token of the card to be charged, as returned from the cards API or customers API.
-        :param payment_source_token: The token of the payment source to be charged, as returned from the payment_sources API.
-        :param customer_token: The token of the customer to be charged, as returned from the customers API.
+        :param email: The email address of the customer.
+        :param first_name: The customer’s first name.
+        :param last_name: The customer’s surname.
+        :param phone_number: The customer’s contact number.
+        :param company: The company associated with the customer.
+        :param notes: Internal notes for the customer.
+        :param card: The full details of the payment card to be stored
+        :param card_token: The token of the card to be stored, as returned from the cards API or customers API.
+        :param primary_card_token: The token of the card to become the customer’s primary card, as returned from the cards API or customers API.
         :return: None
         """
         ...
 
-    def put_charges_charge_token_void(self) -> None:
-        """
-        Voids a previously authorised charge and returns its details.
-        This will return the reserved funds to the cardholder, and capture will no longer be possible.
 
-        PUT /charges/charge-token/void
 
-        Example:
-        curl https://test-api.pinpayments.com/1/charges/ch_lfUYEBK14zotCTykezJkfg/void -u your-secret-api-key: -X PUT
-
-        :return: None
-        """
-        ...
-
-    def put_charges_charge_token_capture(self) -> None:
-        """
-        Captures a previously authorised charge and returns its details.
-        Currently, you can only capture the full amount that was originally authorised.
-
-        PUT /charges/charge-token/capture
-
-        Example:
-        curl https://test-api.pinpayments.com/1/charges/ch_lfUYEBK14zotCTykezJkfg/capture -u your-secret-api-key: -X PUT
-
-        :return: None
-        """
-        ...
-
-    def get_charges(self) -> None:
-        """
-        Returns a paginated list of all charges.
-
-        GET /charges
-
-        Example:
-        curl https://test-api.pinpayments.com/1/charges -u your-secret-api-key:
-
-        :return: None
-        """
-        ...
-
-    def get_charges_search(
-            self,
-            query: Optional[str] = None,
-            start_date: Optional[str] = None,
-            end_date: Optional[str] = None,
-            sort: Optional[str] = None,
-            direction: Optional[int] = None,
-    ) -> None:
-        """
-        Returns a paginated list of charges matching the search criteria.
-
-        GET /charges/search
-
-        Example:
-        curl https://test-api.pinpayments.com/1/charges/search -u your-secret-api-key: -X GET -d "query=test%20charge"
-
-        :param query: Return only charges whose fields match the query. Fields covered include description, email, metadata, cardholder name, currency, amount (exact), charge token (exact), card token (exact), refund token (exact), and customer token (exact).
-        :param start_date: Return only charges created on or after this date (e.g. 2012/12/25).
-        :param end_date: Return only charges created before this date (e.g. 2013/12/25).
-        :param sort: The field used to sort the charges (created_at or amount). Default value is created_at.
-        :param direction: The direction in which to sort the charges (1 for ascending or -1 for descending). Default value is 1.
-        :return: None
-        """
-        ...
-
-    def get_charges_charge_token(self) -> None:
-        """
-        Returns the details of a charge.
-
-        GET /charges/charge-token
-
-        Example:
-        curl https://test-api.pinpayments.com/1/charges/ch_lfUYEBK14zotCTykezJkfg -u your-secret-api-key:
-
-        :return: None
-        """
-        ...
-
-    def get_charges_verify(self) -> None:
-        """
-        Verify the result of a 3D Secure enabled charge.
-        For more information about 3D Secure, see the 3D Secure integration guide.
-
-        GET /charges/verify?session_token=session-token
-
-        Example:
-        curl https://api.pinpayments.com/1/charges/verify -u your-secret-api-key: -X GET -d "session_token=se_sGt9PuNYfVzJqTSLP2CV8g"
-
-        :return: None
-        """
-        ...
 
 
 if __name__ == '__main__':
